c9360d8782c9af91b881e388a2d499fb1d84d4d0
Alema <alema.duvnjak@edu.fit.ba>
2025-11-27 15:50:56 +0100

    Configure DbContext and database connection

---
+++ CafeEase/CafeEase.Services/Database/CafeEaseDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CafeEase.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeEase.Services.Database
+{
+    public class CafeEaseDbContext : DbContext
+    {
+        public CafeEaseDbContext(DbContextOptions<CafeEaseDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<LoyaltyPoints> LoyaltyPoints { get; set; }
+        public DbSet<Recommendation> Recommendations { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
