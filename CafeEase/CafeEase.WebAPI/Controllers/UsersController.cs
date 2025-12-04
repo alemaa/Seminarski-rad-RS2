8224a2df09bace1a5ee3b54fbf15168b0896a2eb
Alema <alema.duvnjak@edu.fit.ba>
2025-12-04 18:22:15 +0100

    Add Users API Controller

---
+++ CafeEase/CafeEase.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using CafeEase.Model.Requests;
+using CafeEase.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeEase.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : BaseCRUDController<Model.User, Model.SearchObjects.UserSearchObject, Model.Requests.UserInsertRequest, Model.Requests.UserUpdateRequest>
+    {
+        public UsersController(ILogger<BaseController<Model.User, Model.SearchObjects.UserSearchObject>> logger, IUserService service) : base(logger, service)
+        {
+        }
+        [AllowAnonymous]
+        public override Task<Model.User> Insert([FromBody] UserInsertRequest insert)
+        {
+            return base.Insert(insert);
+        }
+    }
+}
