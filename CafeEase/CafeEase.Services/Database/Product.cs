7fd63e35da5a3b1c01c666aabfc7e2685a534c76
Alema <alema.duvnjak@edu.fit.ba>
2025-12-06 16:16:18 +0100

    Add image column to product table via migration

--- CafeEase/CafeEase.Services/Database/Product.cs
+++ CafeEase/CafeEase.Services/Database/Product.cs
@@ -10,6 +10,7 @@ namespace CafeEase.Services.Database
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public string? Description { get; set; }
+        public byte[]? Image { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
 
--- CafeEase/CafeEase.Services/Migrations/20251204233815_AddProductImage.Designer.cs
+++ CafeEase/CafeEase.Services/Migrations/20251204233815_AddProductImage.Designer.cs
@@ -4,6 +4,7 @@ using CafeEase.Services.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CafeEase.Services.Migrations
 {
     [DbContext(typeof(CafeEaseDbContext))]
-    partial class CafeEaseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251204233815_AddProductImage")]
+    partial class AddProductImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
