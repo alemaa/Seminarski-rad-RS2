c80fb017ca9d1c2387ee61ce175a094099c53fa5
Alema <alema.duvnjak@edu.fit.ba>
2025-12-06 16:15:11 +0100

    Add image property to product model

--- CafeEase/CafeEase.Model/Product.cs
+++ CafeEase/CafeEase.Model/Product.cs
@@ -10,6 +10,7 @@ namespace CafeEase.Model
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
+        public byte[]? Image { get; set; }
         public int CategoryId { get; set; }
     }
 }
