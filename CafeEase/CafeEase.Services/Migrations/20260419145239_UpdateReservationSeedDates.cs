6acb3ece995a566177fe5967275c9e593710bbd1
Alema <alema.duvnjak@edu.fit.ba>
2026-04-19 17:07:24 +0200

    chore: add migration for updated reservation seed data

---
+++ CafeEase/CafeEase.Services/Migrations/20260419145239_UpdateReservationSeedDates.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeEase.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReservationSeedDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 4, 21, 19, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 4, 22, 20, 30, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 4, 24, 18, 30, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 4, 26, 21, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 2, 5, 19, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 2, 6, 20, 30, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 2, 6, 20, 30, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ReservationDateTime",
+                value: new DateTime(2026, 2, 7, 20, 30, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
