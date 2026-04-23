c097e70716c45b8573b26be4697cc6b2c0a592e1
Alema <alema.duvnjak@edu.fit.ba>
2026-04-23 14:18:53 +0200

    Allow anonymous access to cities endpoint for registration

--- CafeEase/CafeEase.WebAPI/Controllers/CitiesController.cs
+++ CafeEase/CafeEase.WebAPI/Controllers/CitiesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CafeEase.WebAPI.Controllers
 {
+    [AllowAnonymous]
     [Route("api/[controller]")]
     [ApiController]
     public class CitiesController : BaseCRUDController<Model.City, BaseSearchObject, CityUpsertRequest, CityUpsertRequest>
