0e8995b5809196d090aebad2d6236088b1e694c7
Alema <alema.duvnjak@edu.fit.ba>
2026-06-09 11:58:03 +0200

    Ignore null values in partial update mappings

--- CafeEase/CafeEase.Services/MappingProfile.cs
+++ CafeEase/CafeEase.Services/MappingProfile.cs
@@ -8,6 +8,18 @@ namespace CafeEase.Services.Mapping
 {
     public class MappingProfile : Profile
     {
+        private static void IgnoreNullValues<TSource, TDestination>(IMappingExpression<TSource, TDestination> map)
+        {
+            map.ForAllMembers(opt =>
+            {
+                opt.PreCondition((src, dest, context) =>
+                {
+                    var sourceProperty = typeof(TSource).GetProperty(opt.DestinationMember.Name);
+                    return sourceProperty == null || sourceProperty.GetValue(src) != null;
+                });
+            });
+        }
+
         public MappingProfile()
         {
             CreateMap<Database.User, Model.User>();
@@ -18,37 +30,37 @@ namespace CafeEase.Services.Mapping
             CreateMap<CategoryUpsertRequest, Database.Category>();
             CreateMap<Database.Product, Model.Product>();
             CreateMap<ProductInsertRequest, Database.Product>();
-            CreateMap<ProductUpdateRequest, Database.Product>();
+            IgnoreNullValues(CreateMap<ProductUpdateRequest, Database.Product>());
             CreateMap<OrderInsertRequest, Database.Order>();
             CreateMap<Database.Order, Model.Order>().ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => src.User.FirstName + " " + src.User.LastName))
              .ForMember(dest => dest.TableNumber, opt => opt.MapFrom(src => src.Table.Number)); ;
             CreateMap<Database.OrderItem, Model.OrderItem>().ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name));
             CreateMap<OrderItemInsertRequest, Database.OrderItem>();
-            CreateMap<OrderItemUpdateRequest, Database.OrderItem>();
+            IgnoreNullValues(CreateMap<OrderItemUpdateRequest, Database.OrderItem>());
             CreateMap<Database.Payment, Model.Payment>();
             CreateMap<PaymentInsertRequest, Database.Payment>();
-            CreateMap<PaymentUpdateRequest, Database.Payment>();
+            IgnoreNullValues(CreateMap<PaymentUpdateRequest, Database.Payment>());
             CreateMap<Database.LoyaltyPoints, Model.LoyaltyPoints>();
             CreateMap<LoyaltyPointsInsertRequest, Database.LoyaltyPoints>();
-            CreateMap<LoyaltyPointsUpdateRequest, Database.LoyaltyPoints>();
+            IgnoreNullValues(CreateMap<LoyaltyPointsUpdateRequest, Database.LoyaltyPoints>());
             CreateMap<Database.Promotion, Model.Promotion>().ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.PromotionCategories.Where(pc => pc.Category != null).Select(pc => pc.Category)));
             CreateMap<PromotionInsertRequest, Database.Promotion>();
-            CreateMap<PromotionUpdateRequest, Database.Promotion>();
+            IgnoreNullValues(CreateMap<PromotionUpdateRequest, Database.Promotion>());
             CreateMap<Database.Review, Model.Review>().ForMember(d => d.UserFullName, o => o.MapFrom(s => s.User.FirstName + " " + s.User.LastName)).ForMember(d => d.ProductName, o => o.MapFrom(s => s.Product.Name));
             CreateMap<ReviewInsertRequest, Database.Review>();
-            CreateMap<ReviewUpdateRequest, Database.Review>();
+            IgnoreNullValues(CreateMap<ReviewUpdateRequest, Database.Review>());
             CreateMap<Database.Reservation, Model.Reservation>().ForMember(dest => dest.TableNumber, opt => opt.MapFrom(src => src.Table.Number)).
                 ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => src.User != null ? src.User.FirstName + " " + src.User.LastName : null))
                 .ForMember(dest => dest.UserMail, opt => opt.MapFrom(src => src.User != null ? src.User.Email : null));
             CreateMap<ReservationInsertRequest, Database.Reservation>();
-            CreateMap<ReservationUpdateRequest, Database.Reservation>();
+            IgnoreNullValues(CreateMap<ReservationUpdateRequest, Database.Reservation>());
             CreateMap<Database.Recommendation, Model.Recommendation>().ReverseMap();
             CreateMap<Database.Table, Model.Table>();
             CreateMap<TableInsertRequest, Database.Table>();
             CreateMap<TableUpdateRequest, Database.Table>();
             CreateMap<Database.Inventory, Model.Inventory>().ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name));
             CreateMap<InventoryInsertRequest, Database.Inventory>();
-            CreateMap<InventoryUpdateRequest, Database.Inventory>();
+            IgnoreNullValues(CreateMap<InventoryUpdateRequest, Database.Inventory>());
             CreateMap<Database.City, Model.City>();
             CreateMap<CityUpsertRequest, Database.City>();
             CreateMap<Database.Notification, Model.Notification>();
